68ae5117cf2adfb7570d4c62909625eabc5d0dde
vipink28 <vipinkalyan28@gmail.com>
2025-08-11 12:26:58 +0530

    login and register logic added.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,48 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
 const Login = () => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState(null);
+    const handleChange = (e) => {
+        let { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`http://localhost:5000/users?email=${formData.email}&password=${formData.password}`, { method: "GET" });
+            const users = await response.json();
+            if (users.length > 0) {
+                alert("user found, logged in")
+                navigate("/create-invoice")
+            } else {
+                alert("email/password is invalid");
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
     return (
         <div className="py-2">
             <h2>Login</h2>
             <form>
                 <div className="mb-3">
                     <label className="block font-medium mb-3">Email</label>
-                    <input type="email" name="email" className="h-10 px-2 rounded-md focus:outline-none border border-gray-200 w-full" />
+                    <input type="email" name="email" className="h-10 px-2 rounded-md focus:outline-none border border-gray-200 w-full" onChange={handleChange} />
                 </div>
                 <div className="mb-3">
                     <label className="block font-medium mb-3">Password</label>
-                    <input type="password" name="password" className="h-10 px-2 rounded-md focus:outline-none border border-gray-200 w-full" />
+                    <input type="password" name="password" className="h-10 px-2 rounded-md focus:outline-none border border-gray-200 w-full" onChange={handleChange} />
                 </div>
 
-                <button className="bg-slate-300 text-slate-950 px-4 py-2 rounded-md font-bold cursor-pointer">Login</button>
+                <button onClick={handleLogin} className="bg-slate-300 text-slate-950 px-4 py-2 rounded-md font-bold cursor-pointer">Login</button>
             </form>
         </div>
     )
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,23 +1,65 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
 const SignUp = () => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState(null);
+    const handleChange = (e) => {
+        let { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+        //http://localhost:5000/users
+        let config = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        }
+        try {
+            const checkUser = await fetch(`http://localhost:5000/users?email=${formData.email}`, { method: "GET" });
+            const user = await checkUser.json();
+            if (user.length > 0) {
+                alert("email already exist");
+            } else {
+                const response = await fetch("http://localhost:5000/users", config);
+                if (response.ok) {
+                    alert("user created")
+                    navigate("/create-invoice")
+                }
+            }
+
+        } catch (error) {
+            alert(error);
+        }
+    }
+
+
     return (
         <div className="py-2">
             <h2>Sign Up</h2>
             <form>
                 <div className="mb-3">
                     <label className="block font-medium mb-3">Full Name</label>
-                    <input type="text" name="fullname" className="h-10 px-2 rounded-md focus:outline-none border border-gray-200 w-full" />
+                    <input type="text" name="fullname" className="h-10 px-2 rounded-md focus:outline-none border border-gray-200 w-full" onChange={handleChange} />
                 </div>
                 <div className="mb-3">
                     <label className="block font-medium mb-3">Email</label>
-                    <input type="email" name="email" className="h-10 px-2 rounded-md focus:outline-none border border-gray-200 w-full" />
+                    <input type="email" name="email" className="h-10 px-2 rounded-md focus:outline-none border border-gray-200 w-full" onChange={handleChange} />
                 </div>
                 <div className="mb-3">
                     <label className="block font-medium mb-3">Password</label>
-                    <input type="password" name="password" className="h-10 px-2 rounded-md focus:outline-none border border-gray-200 w-full" />
+                    <input type="password" name="password" className="h-10 px-2 rounded-md focus:outline-none border border-gray-200 w-full" onChange={handleChange} />
                 </div>
 
-                <button className="bg-slate-300 text-slate-950 px-4 py-2 rounded-md font-bold cursor-pointer">Sign Up</button>
+                <button onClick={handleSignUp} className="bg-slate-300 text-slate-950 px-4 py-2 rounded-md font-bold cursor-pointer">Sign Up</button>
             </form>
         </div>
     )
